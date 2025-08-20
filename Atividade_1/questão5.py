30421975d0993d2b2f91782308cf4ea99bfcffa8
DAVI FIGUEIREDO DE OLIVEIRA JORGE <brksdavi123456789@gmail.com>
2025-08-20 16:05:36 -0300

    senha de fdp

--- Atividade_1/questão5.py
+++ Atividade_1/questão5.py
@@ -1,8 +1,41 @@
-password = input("Digite a senha: ")
+senha = input("digite sua senha: ")
+i = 0
 
-if password.count("@") < 1 or password.count("#") < 1 or password.count("$") < 1 and len(password) < 8:
-    print("Senha valida")
-else:
-    print("Senha invalida")
-
+while True:
+    if len(senha) <= 8:
+        print("Senha muito curta, digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.isalpha():
+        print("Senha deve conter números, digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.isdigit():
+        print("Senha deve conter letras, digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.islower():
+        print("Senha deve conter letras maiúsculas, digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.isupper():
+        print("Senha deve conter letras minúsculas, digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.count('@') != 1:
+        print("Senha deve conter exatamente um '@', digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.count('#') != 1:
+        print("Senha deve conter exatamente um '#', digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    elif senha.count('$') != 1:
+        print("Senha deve conter exatamente um '$', digite novamente")
+        senha = input("digite sua senha: ")
+        i += 1
+    else:
+        print("Senha válida")
+        break
+        
 
